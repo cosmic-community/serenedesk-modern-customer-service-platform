b4b942c047066f6e653a1a474a69b8aee184c989
Cosmic AI <support@cosmicjs.com>
2025-08-21 12:34:37 -0700

    Remove orange product update announcement banner from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Hero from '@/components/Hero'
 import FeaturedProducts from '@/components/FeaturedProducts'
 import CustomerTestimonials from '@/components/CustomerTestimonials'
-import AnnouncementBanner from '@/components/AnnouncementBanner'
 import FeaturedResources from '@/components/FeaturedResources'
 import AwardsSection from '@/components/AwardsSection'
 import AIServiceSection from '@/components/AIServiceSection'
@@ -9,23 +8,18 @@ import ServiceSolutions from '@/components/ServiceSolutions'
 import { 
   getFeaturedProducts, 
   getCustomerStories,
-  getHomepageAnnouncements,
   getFeaturedResources 
 } from '@/lib/cosmic'
 
 export default async function HomePage() {
-  const [products, customerStories, announcements, resources] = await Promise.all([
+  const [products, customerStories, resources] = await Promise.all([
     getFeaturedProducts(),
     getCustomerStories(),
-    getHomepageAnnouncements(),
     getFeaturedResources()
   ])
 
   return (
     <div>
-      {announcements.length > 0 && (
-        <AnnouncementBanner announcements={announcements} />
-      )}
       <Hero />
       <AwardsSection />
       <AIServiceSection />
